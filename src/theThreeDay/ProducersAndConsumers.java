a50bde3eb2250dbbe7714ddc0e3eb1d057c9305f
zhengjiqi <fightingcrap@gmail.com>
2017-06-11 19:20:17 +0800

    线程学习第三弹  生产者消费者的实现

--- src/theThreeDay/ProducersAndConsumers.java
+++ src/theThreeDay/ProducersAndConsumers.java
@@ -34,25 +34,47 @@ public class ProducersAndConsumers {
         /**
          *一对多的情况模拟
          */
+//        List list=new ArrayList();
+//        Object object=new Object();
+//        Produces produces=new Produces(list,object);
+//        Consumers consumers=new Consumers(list,object);
+//        Thread thread1=new Thread(new Thread1(produces));
+//        Thread thread7=new Thread(new Thread2(consumers));
+//        Thread thread3=new Thread(new Thread2(consumers));
+//        Thread thread4=new Thread(new Thread2(consumers));
+//        Thread thread5=new Thread(new Thread2(consumers));
+//        Thread thread2=new Thread(new Thread2(consumers));
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+//        thread4.start();
+//        thread5.start();
+//        thread7.start();
+
+        //在实验过程中，我们发现最后结果会被阻塞，程序没有继续运行下去，出现了很有名的错误，那就是："假死"，因为在程序运行
+        //的过程中，调用生产者后，又调用生产者，然后调用了两次消费者，就无法解开生产者的等待了，解决方法就是使用notifyAll（）代替notify（）
         List list=new ArrayList();
         Object object=new Object();
-        Produces produces=new Produces(list,object);
-        Consumers consumers=new Consumers(list,object);
-        Thread thread1=new Thread(new Thread1(produces));
-        Thread thread7=new Thread(new Thread2(consumers));
-        Thread thread3=new Thread(new Thread2(consumers));
-        Thread thread4=new Thread(new Thread2(consumers));
-        Thread thread5=new Thread(new Thread2(consumers));
-        Thread thread2=new Thread(new Thread2(consumers));
+        Produces1 produces=new Produces1(list,object);
+        Consumers1 consumers=new Consumers1(list,object);
+        Thread thread1=new Thread(new Thread3(produces));
+        Thread thread7=new Thread(new Thread4(consumers));
+        Thread thread3=new Thread(new Thread4(consumers));
+        Thread thread4=new Thread(new Thread4(consumers));
+        Thread thread5=new Thread(new Thread4(consumers));
+        Thread thread2=new Thread(new Thread4(consumers));
         thread1.start();
         thread2.start();
         thread3.start();
         thread4.start();
         thread5.start();
         thread7.start();
+        //结果和原来一样了，假死的问题也解决了
 
-        //在实验过程中，我们发现最后结果会被阻塞，程序没有继续运行下去，出现了很有名的错误，那就是："假死"，因为在程序运行
-        //的过程中，调用生产者后，又调用生产者，然后调用了两次消费者，就无法解开生产者的等待了，解决方法就是使用notifyAll（）代替notify（）
+        /**
+         * 多对一，和多对多多情况其实差不多，所以就不用演示了，在这里需要注意一下，在程序中，我都是用循环进行判断的，而不是直接用if
+         * 原因就在于，使用if可能会导致线程条件发生改变后，出现错误。也就是不会进行二次情况判断。
+         */
 
     }
 }
@@ -131,6 +153,90 @@ class Thread1 implements Runnable{
 class Thread2 implements Runnable{
     private Consumers consumers;
     public Thread2(Consumers consumers){
+        this.consumers=consumers;
+    }
+    @Override
+    public void run() {
+        while(true){
+            consumers.remove();
+        }
+    }
+}
+
+/**
+ * 修改生产者和消费者，避免假死的情况
+ */
+class Produces1{
+    private List list;
+    private Object object;
+    public Produces1(List list,Object object){
+        this.list=list;
+        this.object=object;
+    }
+    public void add(){
+        synchronized (object){
+            try {
+                while (list.size() != 0) {
+                    object.wait();
+                }
+                list.add("1");
+                System.out.println("生产");
+                Thread.sleep(1000);
+                object.notifyAll();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Consumers1{
+    private List list;
+    private Object object;
+    public Consumers1(List list,Object object){
+        this.list=list;
+        this.object=object;
+    }
+    public void remove(){
+        synchronized (object){
+            try{
+                while(list.size()==0){
+                    object.wait();
+                }
+                list.remove(0);
+                System.out.println("消费");
+                Thread.sleep(1000);
+                object.notifyAll();
+
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
+
+/**
+ * 构建两个线程
+ */
+class Thread3 implements Runnable{
+    private Produces1 produces;
+    public Thread3(Produces1 produces){
+        this.produces=produces;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            produces.add();
+        }
+
+    }
+}
+
+class Thread4 implements Runnable{
+    private Consumers1 consumers;
+    public Thread4(Consumers1 consumers){
         this.consumers=consumers;
     }
     @Override
